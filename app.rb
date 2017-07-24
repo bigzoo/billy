739ba73ec612c5aa0e20dc8bf5c13721117c5464
Chris Nyaga <nyagachris75@gmail.com>
2017-07-24 14:44:41 +0300

    Sessions added to app.rb.

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require('bundler/setup')
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 also_reload('lib/**/*.rb')
-
+enable :sessions
 #Route to the index
 get('/') do
   erb(:index)
