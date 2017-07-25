c1df050bd049398874225a3d97dd00d0134e3423
samwel <samwuelkinuthia@gmail.com>
2017-07-25 08:55:42 +0300

    added comments and get method for company_home route

--- app.rb
+++ app.rb
@@ -1,36 +1,38 @@
-require('bundler/setup')
+facerequire('bundler/setup')
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 also_reload('lib/**/*.rb')
 enable :sessions
-#Route to the index
+# Route to the index
 get('/') do
   erb(:index)
 end
 
+# start of sign up and login methods
+
 get '/user/signup' do
-    erb (:user_signup)
+  erb :user_signup
 end
 
 get '/company/signup' do
-    erb (:company_signup)
+  erb :company_signup
 end
 
-post('/user/signup')do
-@user = User.new(first_name: params["first_name"],last_name: params["last_name"], email: params["email"], password: params["password"])
-@user.save
-session[:id] = @user.id
-redirect '/user/home'
+post('/user/signup') do
+  @user = User.new(first_name: params['first_name'], last_name: params['last_name'], email: params['email'], password: params['password'])
+  @user.save
+  session[:id] = @user.id
+  redirect '/user/home'
 end
 
-post('/company/signup')do
-@user = Company.new(name: params["name"], email: params["email"], password: params["password"])
-@user.save
-session[:id] = @user.id
-redirect '/company/home'
+post('/company/signup') do
+  @user = Company.new(name: params['name'], email: params['email'], password: params['password'])
+  @user.save
+  session[:id] = @user.id
+  redirect '/company/home'
 end
 
-get('/user/home')do
+get('/user/home') do
   if session[:id]
     @user = User.find(session[:id])
     erb(:user_home)
@@ -39,37 +41,46 @@ get('/user/home')do
   end
 end
 
-get('/company/home')do
+get('/company/home') do
   if session[:id]
     @company = Company.find(session[:id])
     erb(:company_home)
   else
     redirect('/company/login')
   end
-
 end
 
-get('/user/login')do
+get('/user/login') do
   erb(:user_login)
 end
 
-get('/company/login')do
+get('/company/login') do
   erb(:company_login)
 end
 
-post('/user')do
-@user = User.find_by(email: params["email"], password: params["password"])
-session[:id] = @user.id
-redirect('/company/home')
+post('/user') do
+  @user = User.find_by(email: params['email'], password: params['password'])
+  session[:id] = @user.id
+  redirect('/company/home')
 end
 
-post('/company')do
-@company = Company.find_by(email: params["email"], password: params["password"])
-session[:id] = @company.id
-redirect('/user/home')
+post('/company') do
+  @company = Company.find_by(email: params['email'], password: params['password'])
+  session[:id] = @company.id
+  redirect('/user/home')
 end
 
-get '/logout'do
+get '/logout' do
   session.clear
   redirect('/')
 end
+
+# end of sign in and login methods
+
+# company_home
+get('/companies/:id') do
+  @company = Company.find(params.fetch('id').to_i)
+  @user_accounts = User_account.all
+  erb(:company_home)
+end
+# end of comopany home
