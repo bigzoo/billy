a556329e5ea6934b5ae7698d586f12c011e76b7a
samwel <samwuelkinuthia@gmail.com>
2017-07-27 15:15:24 +0300

    added preloader effect logic

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -43,3 +43,25 @@ $('.datepicker').pickadate({
               $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
           }
     });
+
+    var preloader;
+
+    function preload(opacity) {
+        if(opacity <= 0.8) {
+            showContent();
+        }
+        else {
+            preloader.style.opacity = opacity;
+            window.setTimeout(function() { preload(opacity - 0.05) }, 100);
+        }
+    }
+
+    function showContent() {
+        preloader.style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+    }
+
+    document.addEventListener("DOMContentLoaded", function () {
+        preloader = document.getElementById('preloader');
+        preload(1);
+    });
